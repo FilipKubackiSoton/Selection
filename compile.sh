084dcc5127aabfa39ca7c28b4825d38368185d42
FilipKubackiSoton <fk1g19@soton.ac.uk>
2022-04-19 14:23:18 +0100

    add loop over scv file with temp files

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-docker run --rm -v "$PWD":/usr/src/myapp gcc:test g++ -Wall -I /bin/gsl/include -L /bin/gsl/lib -O3 *.cpp -lgsl -lgslcblas -lm -o sr
+docker run --rm -v "$PWD":/usr/src/myapp gcc:test g++ -I /bin/gsl/include -L /bin/gsl/lib -O3 *.cpp -lgsl -lgslcblas -lm -o sr
--- main.cpp
+++ main.cpp
@@ -7,6 +7,19 @@
 #include "popsize.h"
 #include <stdio.h>
 #include <dirent.h>
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <string.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <filesystem>
+
+
 
 // void run_selection(std::vector<std::string> arguments){
 // 	//std::vector<std::string> arguments = {"./sr", "-D", file_name, "-G", "25", "-N", "10000", "-n", "500000", "-d", "0.001", "-F", "20", "-f", "1000", "-s", "100", "-P", "constant.pop", "-e", "8067", "-a", "-o", file_name};
@@ -37,6 +50,26 @@ void run_test(std::string file_name){
 	settings mySettings((int)cstrings.size(), cstrings.data());
 	MbRandom* r = new MbRandom(mySettings.get_seed());
 	mcmc myMCMC(mySettings, r);
+}
+
+// trim from left
+inline std::string& ltrim(std::string& s, const char* t = " \t\n\r\f\v")
+{
+    s.erase(0, s.find_first_not_of(t));
+    return s;
+}
+
+// trim from right
+inline std::string& rtrim(std::string& s, const char* t = " \t\n\r\f\v")
+{
+    s.erase(s.find_last_not_of(t) + 1);
+    return s;
+}
+
+// trim from left & right
+inline std::string& trim(std::string& s, const char* t = " \t\n\r\f\v")
+{
+    return ltrim(rtrim(s, t), t);
 }
 
 bool hasEnding (std::string const &fullString, std::string const &ending) {
@@ -75,41 +108,143 @@ int main (int argc, char * const argv[]) {
 		if (mySettings.get_linked()) {
 			
 		} else {
-			std::vector<std::string> s = {"data/1.txt", "data/2.txt", "data/3.txt"};
-			struct dirent *entry = nullptr;
-			DIR *dp = nullptr;
 
-			dp = opendir("data/");
-			if (dp != nullptr) {
-				while ((entry = readdir(dp))){
-					std::string filename = entry->d_name;
-					if(hasEnding(filename, ".txt")){
-						printf ("%s\n", entry->d_name);
-						filename = "data/"+filename;
-						std::vector<std::string> arguments = {"./sr", "-D", filename, "-G", "25", "-N", "10000", "-n", "500000", "-d", "0.001", "-F", "20", "-f", "1000", "-s", "100", "-P", "constant.pop", "-e", "8067", "-a", "-o", filename};
-
-						std::vector<char*> argvv;
-						for (const auto& arg : arguments)
-							argvv.push_back((char*)arg.data());
-						argvv.push_back(nullptr);
-
-						std::cout<< argvv[2]<<std::endl;
-						
-						settings mySettings(argvv.size() - 1, argvv.data());
-						MbRandom* rr = new MbRandom(mySettings.get_seed());
-						//argv['FILENMAE'] = FILE
-						//settings mySettings(argc, argv);
-						//MbRandom* r = new MbRandom(mySettings.get_seed());
-
-
-						mcmc myMCMC(mySettings, rr);
-					}
-						
+			std::string fname = "input_1000.csv";
+			//cout<<"Enter the file name: ";
+			//cin>>fname;
+		
+			std::vector<std::vector<std::string>> content;
+			std::vector<std::string> row;
+			std::string line, word;
+		
+			std::fstream file (fname, std::ios::in);
+			if(file.is_open())
+			{
+				while(std::getline(file, line))
+				{
+					row.clear();
+		
+					std::stringstream str(line);
+		
+					while(std::getline(str, word, ','))
+						row.push_back(trim(word));
+					content.push_back(row);
 				}
-					
 			}
+			else
+				std::cout<<"Could not open the file\n";
 
-			closedir(dp);
+
+			for(int i=1;i<content.size();i++)
+				{
+					// write to file 
+
+					std::string filename = content[i][1]+".txt";
+					std::ofstream MyFile(filename);
+					MyFile << content[i][2] << "\t" << content[i][2+1] 		<< "\t" << -6123 	<< "\t" << -5500 << "\n";
+					MyFile << content[i][4] << "\t" << content[i][4+1] 		<< "\t" << -5535 	<< "\t" << -4906 << "\n";
+					MyFile << content[i][6] << "\t" << content[i][6+1] 		<< "\t" << -5051 	<< "\t" << -4240 << "\n";
+					MyFile << content[i][8] << "\t" << content[i][8+1] 		<< "\t" << -4507 	<< "\t" << -3935 << "\n";
+					MyFile << content[i][10] << "\t" << content[i][10+1] 	<< "\t" << -4001 	<< "\t" << -3461 << "\n";
+					MyFile << content[i][12] << "\t" << content[i][12+1] 	<< "\t" << -3517 	<< "\t" << -3013 << "\n";
+					MyFile << content[i][14] << "\t" << content[i][14+1] 	<< "\t" << -3044 	<< "\t" << -2471 << "\n";
+					MyFile << content[i][16] << "\t" << content[i][16+1] 	<< "\t" << -2527 	<< "\t" << -2045 << "\n";
+					MyFile << content[i][18] << "\t" << content[i][18+1] 	<< "\t" << -1947 	<< "\t" << -1550 << "\n";
+					MyFile << content[i][20] << "\t" << content[i][20+1] 	<< "\t" << -1504 	<< "\t" << -971 << "\n";
+					MyFile << content[i][22] << "\t" << content[i][22+1] 	<< "\t" << -1094 	<< "\t" << -492 << "\n";
+					MyFile << content[i][24] << "\t" << content[i][24+1] 	<< "\t" << -400 	<< "\t" << -400 << "\n";
+					MyFile.close();
+
+					// compute 
+					std::vector<std::string> arguments = {"./sr", "-D", filename, "-G", "25", "-N", "10000", "-n", "500000", "-d", "0.001", "-F", "20", "-f", "1000", "-s", "100", "-P", "constant.pop", "-e", "8067", "-a", "-o", content[i][1]};
+
+					std::vector<char*> argvv;
+					for (const auto& arg : arguments)
+						argvv.push_back((char*)arg.data());
+					argvv.push_back(nullptr);
+
+					std::cout<< argvv[2]<<std::endl;
+					
+					settings mySettings(argvv.size() - 1, argvv.data());
+					MbRandom* rr = new MbRandom(mySettings.get_seed());
+					mcmc myMCMC(mySettings, rr);
+
+					// remove file 
+
+					char * tab2 = new char [filename.length()+1];
+					strcpy (tab2, filename.c_str());
+
+					if( std::remove( tab2 ) != 0 )
+						std::perror( "Error deleting file" );
+					else
+						std::puts( "File successfully deleted" );
+					
+				}
+		  	// std::string line, word;
+			
+			// std::ifstream infile("input_1000.csv");
+
+			// while (getline(infile, line,'\n'))
+			// {
+				
+			// stringstream str(line);
+
+			// 	std::ofstream MyFile("filename.txt");
+			// 	while(getline(str, word, ',')){
+			// 		std::cout<< word<<std::endl;
+			// 	}
+			// 	break;
+
+			// 	MyFile << line;
+			// 	MyFile.close();
+			// 	std::cout << line << std::endl;
+			// 	break;
+			// }
+
+
+
+
+
+			// std::vector<std::string> s = {"data/1.txt", "data/2.txt", "data/3.txt"};
+			// struct dirent *entry = nullptr;
+			// DIR *dp = nullptr;
+
+			// dp = opendir("data/");
+			// if (dp != nullptr) {
+			// 	while ((entry = readdir(dp))){
+			// 		std::string filename = entry->d_name;
+			// 		if(hasEnding(filename, ".txt")){
+			// 			printf ("%s\n", entry->d_name);
+			// 			filename = "data/"+filename;
+			// 			std::vector<std::string> arguments = {"./sr", "-D", filename, "-G", "25", "-N", "10000", "-n", "500000", "-d", "0.001", "-F", "20", "-f", "1000", "-s", "100", "-P", "constant.pop", "-e", "8067", "-a", "-o", filename};
+
+			// 			std::vector<char*> argvv;
+			// 			for (const auto& arg : arguments)
+			// 				argvv.push_back((char*)arg.data());
+			// 			argvv.push_back(nullptr);
+
+			// 			std::cout<< argvv[2]<<std::endl;
+						
+			// 			settings mySettings(argvv.size() - 1, argvv.data());
+			// 			MbRandom* rr = new MbRandom(mySettings.get_seed());
+			// 			//argv['FILENMAE'] = FILE
+			// 			//settings mySettings(argc, argv);
+			// 			//MbRandom* r = new MbRandom(mySettings.get_seed());
+
+
+			// 			mcmc myMCMC(mySettings, rr);
+			// 		}
+						
+			// 	}
+					
+			// }
+
+			// closedir(dp);
+
+
+
+
+
 // 			//return 0;
 // 			for(int i = 0; i <= 2; i++){
 				
