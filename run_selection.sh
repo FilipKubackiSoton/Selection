5d6b4ddee469a3a48a378f9ba2ae724d6de715fb
FilipKubackiSoton <fk1g19@soton.ac.uk>
2022-04-21 18:00:34 +0100

    add argument less main

--- run_selection.sh
+++ run_selection.sh
@@ -10,7 +10,7 @@ task(){
     #echo "$1"; sleep 2.0; 
 }
 
-./sr -D tmp -G 25 -N 10000 -n 500000 -d 0.001 -F 20 -f 1000 -s 100 -P constant.pop -e 8067 -a -o tmp  #&
+./sr #-D tmp -G 25 -N 10000 -n 500000 -d 0.001 -F 20 -f 1000 -s 100 -P constant.pop -e 8067 -a -o tmp  #&
 
 # N=4
 # for filename in ./data/*.txt; do
